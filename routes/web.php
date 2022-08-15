e61c9e4a5ce2ce1cce2861b6a1a48d68d5e05a9f
Isadoralucena <66965789+Isadoralucena@users.noreply.github.com>
2022-08-15 17:40:05 -0300

    Adicionado rotas iniciais

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,41 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('HomePage');
+});
+
+Route::get('/login', function () {
+    return view('loginScreen');
+});
+Route::post('/login', function () {
+    return view('loginScreen');
+});
+
+Route::get('/register', function () {
+    return view('registerScreen');
+});
+Route::post('/register', function () {
+    return view('registerScreen');
+});
+
+Route::get('/HomeUser', function () {
+    return view('UserScreen');
+});
+Route::post('/HomeUser', function () {
+    return view('UserScreen');
+});
+
+Route::get('/RegisterContent', function () {
+    return view('RegisterContent');
+});
+Route::post('/RegisterContent', function () {
+    return view('RegisterContent');
+});
+
+Route::get('/error', function () {
+    return view('error');
+});
+
+Route::get('/ConfirmeRegister', function () {
+    return view('ConfirmeRegister');
 });
