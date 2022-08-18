cf251795f658eb12e113c021c12ece68b649d672
mfabricia <110173706+mfabricia@users.noreply.github.com>
2022-08-18 08:38:15 -0300

    Add files via upload
    criando novas páginas e colocando os forms delas para funcionar

---
+++ resources/views/registerScreen.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+</head>
+<body>
+    <h1>Cadastre um usuário!</h1>
+    <form action="{{url('/register')}}" method="POST">
+    @csrf
+        <label for="nome">Nome: </label><input type="text" name="nome" placeholder="Nome">
+        <label for="sobrenome">Sobrenome: </label><input type="text" name="sobrenome" placeholder="Sobrenome">
+        <label for="date">Data de nascimento: </label><input type="date" name="date" placeholder="Data de nascimento">
+        <label for="email">E-mail: </label><input type="email" name="email" placeholder="abc@escolar.ifrn.edu.br">
+        <label for="pass">Senha: </label><input type="password" name="pass" placeholder="Senha">
+        <button>Registrar</button>
+    </form>
+</body>
+</html>
