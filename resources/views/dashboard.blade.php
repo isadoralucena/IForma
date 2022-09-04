9b129cb72b20878ae48f9cda3ccc68d8bee63a8c
Isadoralucena <isadorabeatriz595@gmail.com>
2022-09-04 18:17:01 -0300

    Ajeitei a exibição do autor de cada post
    - não estava aparecendo antes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,10 +4,10 @@
 <h1>Dashboard</h1>
 
 @foreach ($contents as $item)
-    <div style="border: 1px solid red; width: 80%; height: 100px; overflow: hidden; margin-top: 10px;">
+    <div style="border: 1px solid red; width: 80%; overflow: hidden; margin-top: 10px;">
         <h1 style="margin: 5px 5px 5px 5px;padding: 0;">{{$item->title}}</h1>
         <p style="margin: 0px">{{$item->text}}</p>
-        <p>Autor: {{$item->user->name}}</p>
+        <b>Autor: {{$item->user->name}}</b>
     </div>
 
 @endforeach
