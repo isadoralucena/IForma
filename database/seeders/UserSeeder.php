49e712799b221895e1c9a9dbb575d6131e0d3236
Isadoralucena <isadorabeatriz595@gmail.com>
2022-09-04 20:00:20 -0300

    Added a user seeder class

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Administrator',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make(123123123),
+            'userType' => 3,
+            'date' => '2022-01-01'
+        ]);
+    }
+}
