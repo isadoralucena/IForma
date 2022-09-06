a124e74978f1e83dc57808dceca1e4567256432d
beatriz34 <be_atriz34@hotmail.com>
2022-09-06 01:07:00 -0300

    login quase concluído

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,16 +8,33 @@
     <link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css" > 
 </head>
 <body>
-    <div class="padding center texts ">
-        <h1 class="titles">Login</h1>
+            <div class ="login"> 
+                <div class=" center">
+                    <center >                  
+                    <img  onclick="window.location='{{ url('/')}}'" class="imgLogo" src="{{ asset('images/logo.png') }}">
+                </center>
+                </div>
+            
+            </div>
+
+            
+<div class="login3">
+    <div class="padding center texts2 login2">
+
+        <img  onclick="window.location='{{ url('/')}}'" class="imgLogin" src="{{ asset('images/logo.png') }}">
+            
+        <h1 class="titles2 logLogin">Login</h1>
         <form action="{{url('/login')}}" method="POST">
             @csrf
-            <label for="email">E-mail: </label>
-            <input required type="email" name="email" placeholder="nome@escolar.ifrn.edu.br">
-            <label for="password">Senha: </label>
-            <input required type="password" name="password" placeholder="Senha">
-            <button class="littleButton">Acessar</button>
+            <label for="email"><b>E-mail: </b></label>
+            <input required type="email" name="email" placeholder="nome@escolar.ifrn.edu.br" class="inputLogin">
+            <label for="password"><b>Senha: </b> </label>
+            <input required type="password" name="password" placeholder="Senha" class="inputLogin">
+            <button class="littleButton buttonLogin">Acessar</button>
         </form>
     </div>
+</div>
+    
 </body>
+    
 </html>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,10 @@
 </head>
 <body> 
     <div class="center texts ">
+     <img  onclick="window.location='{{ url('/')}}'" class="imgLogo" src="{{ asset('images/logo.png') }}">
+            
         <h1 class="titles">Bem-vindo</h1>
+        <h1>IForma, a plataforma que informa</h1>
         <form action="{{url('/login')}}" method="GET">
             <button class="littleButton">Login</button>            
         </form>
